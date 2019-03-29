89f354dec61f94e4f69bb2beb6b30a1706fc660b
William Li <twelvehertz@gmail.com>
2019-03-29 16:35:28 +1300

    obstacles spawn in random locations

--- src/Map.java
+++ src/Map.java
@@ -1,8 +1,8 @@
 import java.awt.*;
+import java.util.Random;
 
 public class Map {
 
-//    Shape[][] map = new Shape[32][24];
     TileShape[][] map = new TileShape[32][24];
     int tileSize = 40;
 
@@ -10,16 +10,17 @@ public class Map {
         for (int i = 0; i < 32; i++) {
             for (int j = 0; j < 24; j++) {
                 if (i == 0 | i == 31 | j == 0 | j == 23) {
-//                    map[i][j] = new Shape(i*tileSize, j*tileSize, tileSize, tileSize, Color.BLACK, true);
-                    map[i][j] = new TileShape(i*tileSize, j*tileSize,"tree.png", true);
-                } else if (i % 8 == 0 & j % 8 == 0) {
-//                    map[i][j] = new Shape(i*tileSize, j*tileSize, tileSize, tileSize, Color.DARK_GRAY, true);
                     map[i][j] = new TileShape(i*tileSize, j*tileSize,"tree.png", true);
                 } else {
                     map[i][j] = new TileShape(i*tileSize, j*tileSize,"grass.jpg", true);
-//                    map[i][j] = new Shape(i*tileSize, j*tileSize, tileSize, tileSize, Color.GREEN, true);
                 }
             }
+        }
+
+        for (int k = 0; k < 20; k++){
+            int x = new Random().nextInt(32);
+            int y = new Random().nextInt(24);
+            map[x][y] = new TileShape(x*tileSize, y*tileSize,"tree.png", true);
         }
     }
 
